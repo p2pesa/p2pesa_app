b0941c50223b2a044b1fb9ce0b31a8c6b69acf31
RuthKerubo <ruthkeggy@gmail.com>
2021-06-06 00:50:46 +0300

    Create login.dart

---
+++ lib/login.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NIPESA'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 50.0,
+              child: Image.asset(
+                "assets/icon/logo.jpg",
+                fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  // border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  //  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              textColor: Color(0xFFE8EAF6),
+              child: Text('Forgot Password'),
+            ),
+            Container(
+                height: 50,
+                padding: EdgeInsets.only(bottom: 5),
+                child: RaisedButton(
+                  textColor: Color(0xFF01579B),
+                  color: Color(0xFF1DE9B6),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text('Login'),
+                  onPressed: () {
+                    print(usernameController.text);
+                    print(passwordController.text);
+                  },
+                )),
+            Container(
+                child: Row(
+              children: <Widget>[
+                Text("Don't have an a account?"),
+                FlatButton(
+                  textColor: Colors.white,
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
